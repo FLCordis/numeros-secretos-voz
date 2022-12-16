001ebc0f02a80a1c4145dd6d61d0b303791f2ef3
FLCordis <flavio.cordis@hotmail.com>
2022-12-16 15:31:44 -0300

    Number Validation + Attempts

---
+++ app/validacaoChute.js
@@ -0,0 +1,37 @@
+function validacaoChute(chute){
+    const numero = +chute;
+
+    if(chuteInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor Inválido: Não é um número. </div>';
+        return
+    }
+
+    if (numeroForaAlcance(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fora do valor de ${menorValor} e ${maiorValor}</div>`;
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número é menor <i class="fa-solid fa-angles-down"></i></div>
+        `
+    } else{
+        elementoChute.innerHTML += `
+        <div>O número é maior <i class="fa-solid fa-angles-up"></i></div>
+        `
+    }
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForaAlcance(numero){
+    return numero > maiorValor || numero < menorValor;
+}
