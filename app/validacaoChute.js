6dd413e3ee9ee591215a1ec35a716cb38761d25e
FLCordis <flavio.cordis@hotmail.com>
2022-12-16 15:44:59 -0300

    Restart Button + Final Changes

--- app/validacaoChute.js
+++ app/validacaoChute.js
@@ -16,6 +16,8 @@ function validacaoChute(chute){
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class ="btn-restart">Jogar novamente!</button>
         `
     } else if(numero > numeroSecreto){
         elementoChute.innerHTML += `
@@ -35,3 +37,9 @@ function chuteInvalido(numero) {
 function numeroForaAlcance(numero){
     return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente'){
+        window.location.reload();
+    }
+})
